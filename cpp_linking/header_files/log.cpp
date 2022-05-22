754012757a842e6f0a66dc21de3bab066e89c1d6
eduAlexandrPirogov <edualexandrpirogov@gmail.com>
2022-05-22 14:34:44 -0500

    added source code;

---
+++ cpp_linking/header_files/log.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+void Log(const char* message, int x)
+{
+   std::cout << message << "\nx = " << x << '\n';
+};
---
+++ cpp_linking/header_files/main.cpp
@@ -0,0 +1,10 @@
+#include "x.h"
+
+void Log(const char* message, int x);
+
+int main()
+{
+  
+   Log("Hello, world\n", x);
+   return 0;
+};
